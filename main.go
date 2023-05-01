6bed1b3e89e28daf5fbec28e2ea4e3cbee160f26
Alexander <alexander.garcia@smartla.net>
2023-05-01 11:07:06 -0600

    Siguiente commit

--- main.go
+++ main.go
@@ -7,5 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Println(ejercicios.DevuelveVariables("50"))
+	/*estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es: ", os)
+	case "windows":
+		fmt.Println("Esto es: ", os)
+	default:
+		fmt.Println("Sistema desconocido", os)
+	}*/
+	numero, texto := ejercicios.DevuelveVariables("500")
+	fmt.Println(numero)
+	fmt.Println(texto)
 }
